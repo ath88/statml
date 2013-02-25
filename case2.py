eec0ccb44b1ab1ba0b44f1f69539df86206c74c2
Andreas <bock@andreasbock.dk>
2013-02-25 21:57:53 +0100

    case 2: II.1.1: halfway through, need to fix dim in test set

--- case2.py
+++ case2.py
@@ -13,16 +13,41 @@ from PIL import Image
 import scipy.io
 
 ## Processing the data
+
 # Matrix from bodyfat.mat
 rawData = scipy.io.loadmat('Data/bodyfat.mat')['data']
 
-# Selection 1 - columns 4, 7, 8, 9
-sel1 = [[row[3], row[6], row[7], row[8]] for row in rawData]
+# Take 80% of rawData as training set and 20% as test set
+division = math.ceil(len(np.matrix(rawData))*0.8)
+trainingSet = np.random.permutation(rawData)[:division]
+testSet 	= np.random.permutation(rawData)[division:]
 
-# Selection 2 - columns
-sel2 = [row[2] for row in sel1]
+t = np.array([row[1] for row in trainingSet])
 
 # II.1.1 Maximum likelihood solution
 
-def y(x,w):
+# Construct design matrices
 
+# Linear model y
+def y(x,w):
+	# Check dimensions
+	if len(x) != len(w) - 1:
+		raise Exception("Wrong dimensionality of parameters (requires w-1 = x)")
+	result = w[0]
+	for i in range(0,len(x)):
+		result += np.dot(w[i+1], x[i])
+	return result
+
+# Selection 1 (columns 4, 7, 8, 9) arranged as rows
+design1 = np.matrix([[row[3], row[6], row[7], row[8]] for row in trainingSet])
+# Selection 2 (column 8 transposed)
+design2 = np.matrix([row[7] for row in trainingSet])
+
+# Compute ML estimate (training)
+w_ml_sel1 = t * np.linalg.pinv(design1).T # Why am I doing this? Alignment error if not transposed.
+w_ml_sel2 = t * np.linalg.pinv(design2)
+
+# Apply each model to the test set
+print len(testSet)
+print (w_ml_sel1)
+#linmod1 = y(testSet, w_ml_sel1)
